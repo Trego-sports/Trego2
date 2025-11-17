13f98f54eb4c867adc5968f824187a4e0d213bb0
Harry Liu <yxliu@uwaterloo.ca>
2025-11-17 16:44:17 -0500

    update comment

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -37,7 +37,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="light">
       <head>
-        <!-- Google tag (gtag.js) -->
+        {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y1D90MZP2D"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
